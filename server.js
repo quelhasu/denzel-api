7b6955b568298f2e42bc493c1c46d03491d0db06
quelhasu <uquelhas@gmail.com>
2019-03-05 18:52:31 +0100

    :fire: server db connection

--- server.js
+++ server.js
@@ -6,6 +6,24 @@ const config = require('./db');
 const port = 9292;
 const app = express();
 
+const DENZEL_IMDB_ID = 'nm0000243';
+const DATABASE_NAME = 'denzeldb';
+
+const client = mongodb.MongoClient;
+
+var database, collection;
+
+client.connect(config.DB, function (err, db) {
+  if (err) {
+    console.log('database is not connected')
+  }
+  else {
+    database = db.db(DATABASE_NAME);
+    collection = database.collection("movies");
+    console.log(`connected to ${DATABASE_NAME}!!`)
+  }
+});
+
 
 app.listen(port);
 console.log(`Server Running at localhost:${port}`);
