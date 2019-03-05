d08bd9d1f358643f06df3849140a2952d572552e
quelhasu <uquelhas@gmail.com>
2019-03-05 18:53:19 +0100

    :sparkles: /movies-all

--- server.js
+++ server.js
@@ -42,6 +42,15 @@ app.get('/movies/populate', (req, res) => {
   });
 })
 
+app.get('/movies-all', (req, res) => {
+  collection.find({}).toArray((error, result) => {
+        if(error) {
+            return res.status(500).send(error);
+        }
+        res.send(result);
+    });
+})
+
 
 app.listen(port);
 console.log(`Server Running at localhost:${port}`);
