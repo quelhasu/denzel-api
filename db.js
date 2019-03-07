bc2ac5d6fe9acc9793bf38e16d8de7d9e9e2a07c
quelhasu <uquelhas@gmail.com>
2019-03-07 07:52:14 +0100

    :fire: improve database

--- db.js
+++ db.js
@@ -1,4 +1,24 @@
+const mongodb = require('mongodb');
+const client = mongodb.MongoClient;
+const URL = 'mongodb://localhost:27017/denzel';
+const DATABASE_NAME = 'denzeldb'
+
+
+var _db;
+
 module.exports = {
-  DB: 'mongodb://localhost:27017/denzel',
-  DATABASE_NAME: 'denzeldb'
-}
+  connectToMongo : function (callback) {
+    client.connect(URL, {useNewUrlParser: true}, (err, dbase) => {
+      if(err) console.log('database is not connected')
+      else{
+        _db = dbase.db(DATABASE_NAME);
+        console.log(`connected to ${DATABASE_NAME}!!`);
+        return callback( err );
+      }
+    })
+  },
+
+  getDB: function(){
+    return _db;
+  }
+};
--- server.js
+++ server.js
@@ -1,114 +1,102 @@
 const express = require('express');
-const mongodb = require('mongodb');
 const imdb = require('./sandbox').Sandbox;
 const bodyParser = require('body-parser')
 const graphqlHTTP = require('express-graphql');
-const {GraphQLSchema} = require('graphql');
+const { GraphQLSchema } = require('graphql');
 
-const {queryType} = require('./src/query.js');
-const config = require('./db');
+var mongoUtil = require('./db.js');
 
 const port = 9292;
 const app = express();
 
 const DENZEL_IMDB_ID = 'nm0000243';
-const client = mongodb.MongoClient;
 
-const schema = new GraphQLSchema({ query: queryType });
+mongoUtil.connectToMongo(err => {
+  const { queryType } = require('./src/query.js');
+  const schema = new GraphQLSchema({ query: queryType });
 
-var database, collection;
+  var collection = mongoUtil.getDB().collection("movies");
 
-app.use(bodyParser.json());
+  app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: true,
-}));
+  app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    graphiql: true
+  }));
 
-
-client.connect(config.DB, function (err, db) {
-  if (err) {
-    console.log('database is not connected')
+  app.get('/hello', (req, res) => {
+    res.json({ "hello": "world" });
   }
-  else {
-    database = db.db(config.DATABASE_NAME);
-    collection = database.collection("movies");
-    console.log(`connected to ${config.DATABASE_NAME}!!`)
-  }
-});
+  );
 
-app.get('/hello', (req, res) => {
-  res.json({ "hello": "world" });
-}
-);
-
-app.get('/movies/populate', (req, res) => {
-  imdb.getMovies(DENZEL_IMDB_ID).then(movies => {
-    collection.insertMany(movies, (error, result) => {
-      if(error) {
+  app.get('/movies/populate', (req, res) => {
+    imdb.getMovies(DENZEL_IMDB_ID).then(movies => {
+      collection.insertMany(movies, (error, result) => {
+        if (error) {
           return res.status(500).send(error);
-      }
-      res.json(result.result);
-  });
-  });
-})
-
-app.get('/movies-all', (req, res) => {
-  collection.find({}).toArray((error, result) => {
-        if(error) {
-            return res.status(500).send(error);
         }
-        res.send(result);
+        res.json(result.result);
+      });
     });
-})
-
-app.get('/movies', (req, res) => {
-  collection.aggregate([
-    {"$match": { "metascore": { "$gte": 77 } }},
-    {"$sample":{"size":1}}
-  ]).toArray((error, result) => {
-    if(error) {
-        return res.status(500).send(error);
-    }
-    // var awesome = result.filter(movie => movie.metascore >= 77);
-    res.send(result);
-});
-})
-
-
-app.get('/movies/search', (req, res) => {
-  var limit = Number(req.query.limit) || 5;
-  var metascore = Number(req.query.metascore) || 0;
-  if(limit || metascore){
-    collection.aggregate([
-      {"$match": { "metascore": { "$gte": metascore } }},
-      {"$sample": {"size": limit}}
-    ]).toArray((err, result) => {
-      if(err) return res.status(500).send(err);
-      res.json({'limit': limit, 'metascore': metascore, 'results': result});
-    })
-  }
-})
-
-app.get('/movies/:id', (req, res) => {
-  var id = req.params.id;
-  collection.findOne({"id": id}, (err, result) => {
-    if (err) return res.status(500).send(error);
-    if(result) res.send(result);
-    else res.json({"error":`${id} movie does not exist!`});
-  });
-})
-
-app.post('/movies/:id', (req, res) => {
-  var id = req.params.id;
-  console.log(req.body);
-  collection.updateOne({"id":id}, {"$set":{"review":req.body}}, (err, result) => {
-    if(err) return res.status(500).send(error);
-    console.log(result.result);
-    res.end("ok");
   })
-})
+
+  app.get('/movies-all', (req, res) => {
+    collection.find({}).toArray((error, result) => {
+      if (error) {
+        return res.status(500).send(error);
+      }
+      res.send(result);
+    });
+  })
+
+  app.get('/movies', (req, res) => {
+    collection.aggregate([
+      { "$match": { "metascore": { "$gte": 77 } } },
+      { "$sample": { "size": 1 } }
+    ]).toArray((error, result) => {
+      if (error) {
+        return res.status(500).send(error);
+      }
+      // var awesome = result.filter(movie => movie.metascore >= 77);
+      res.send(result);
+    });
+  })
 
 
-app.listen(port);
-console.log(`Server Running at localhost:${port}`);
+  app.get('/movies/search', (req, res) => {
+    var limit = Number(req.query.limit) || 5;
+    var metascore = Number(req.query.metascore) || 0;
+    if (limit || metascore) {
+      collection.aggregate([
+        { "$match": { "metascore": { "$gte": metascore } } },
+        { "$sample": { "size": limit } }
+      ]).toArray((err, result) => {
+        if (err) return res.status(500).send(err);
+        res.json({ 'limit': limit, 'metascore': metascore, 'results': result });
+      })
+    }
+  })
+
+  app.get('/movies/:id', (req, res) => {
+    var id = req.params.id;
+    collection.findOne({ "id": id }, (err, result) => {
+      if (err) return res.status(500).send(error);
+      if (result) res.send(result);
+      else res.json({ "error": `${id} movie does not exist!` });
+    });
+  })
+
+  app.post('/movies/:id', (req, res) => {
+    var id = req.params.id;
+    console.log(req.body);
+    collection.updateOne({ "id": id }, { "$set": { "review": req.body } }, (err, result) => {
+      if (err) return res.status(500).send(error);
+      console.log(result.result);
+      res.end("ok");
+    })
+  })
+
+
+  app.listen(port);
+  console.log(`Server Running at localhost:${port}`);
+});
