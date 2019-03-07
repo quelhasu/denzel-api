44412f341c125cd36a69d1232561c08f69b284b8
quelhasu <uquelhas@gmail.com>
2019-03-07 08:17:05 +0100

    :sparkles: get movie by ID / get awesome movie

--- src/query.js
+++ src/query.js
@@ -1,7 +1,15 @@
-const { GraphQLObjectType,
-  GraphQLString
+var mongoUtil = require('../db.js');
+var collection = mongoUtil.getDB().collection("movies");
+
+const { 
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLList
 } = require('graphql');
 
+const {movieType} = require('./type.js');
 
 //Define the Query
 const queryType = new GraphQLObjectType({
@@ -13,8 +21,21 @@ const queryType = new GraphQLObjectType({
           resolve: function () {
               return "Hello World";
           }
+      },
+      movie: {
+          type: new GraphQLList(movieType),
+          args: {
+              id: { type: GraphQLString}
+          },
+          resolve: function (source, args) {
+            if(args.id) return collection.find({"id": args.id}).toArray();
+            else return collection.aggregate([
+              { "$match": { "metascore": { "$gte": 77 } } },
+              { "$sample": { "size": 1 } }
+            ]).toArray();
+          }
       }
-  }
+    }
 });
 
 exports.queryType = queryType;
