8125168c773dbd885c7288d8de783eab65ec4507
quelhasu <uquelhas@gmail.com>
2019-03-12 17:00:15 +0100

    :sparkles: mongodb atlas

--- src/db.js
+++ src/db.js
@@ -1,15 +1,17 @@
 const mongodb = require('mongodb');
+require('dotenv').config()
+
 const client = mongodb.MongoClient;
 // const URL = 'mongodb://localhost:27017/denzel';
-const URL = 'mongodb://mongo/denzel';
+// const URL = 'mongodb://mongo/denzel';
 const DATABASE_NAME = 'denzeldb'
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0-zidja.mongodb.net/test?retryWrites=true`;
 
 var _db;
 
 module.exports = {
   connectToMongo : function (callback) {
-    client.connect(URL, {useNewUrlParser: true}, (err, dbase) => {
+    client.connect(uri, {useNewUrlParser: true}, (err, dbase) => {
       if(err) console.log('database is not connected')
       else{
         _db = dbase.db(DATABASE_NAME);
